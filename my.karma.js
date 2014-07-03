686710a14ee83ec12fdb042b7311212e2023cd7c
Alejandro Such <alejandro.such@gmail.com>
2014-07-04 01:35:58 +0200

    Changes in karma config and package.json

--- my.karma.js
+++ my.karma.js
@@ -38,7 +38,7 @@ module.exports = function (config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress', 'html'],
+        reporters: ['progress'],
 
         htmlReporter: {
             outputDir: 'karma_html'            //templatePath: __dirname+'/jasmine_template.html'
