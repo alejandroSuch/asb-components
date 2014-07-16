bf9ec4ee754065c68fc0f889109d364a538a0aae
Alejandro Such Berenguer <alejandro.such@gmail.com>
2014-07-17 00:16:49 +0200

    Gruntfile.js created

---
+++ Gruntfile.js
@@ -0,0 +1,73 @@
+'use strict';
+
+module.exports = function (grunt) {
+    // load all grunt tasks matching the `grunt-*` pattern
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        watch: {
+            asbComponents: {
+                files: ['src/**/*.js', 'test/**/*.js'],
+                tasks: ['runAll'],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+
+        jshint: {
+            asbComponents: ['src/**/*.js']
+        },
+
+        clean: {
+            asbComponents: ['asb-components.js', 'asb-components.clean.js', 'asb-components.min.js']
+        },
+
+        concat: {
+            asbComponents: {
+                src: [
+                    ['src/**/*.js']
+                ],
+                dest: 'asb-components.js'
+            }
+        },
+
+        groundskeeper: {
+            asbComponents: {
+                files: {
+                    'asb-components.clean.js': 'asb-components.js' // 1:1 compile
+                }
+            }
+        },
+
+        uglify: {
+            asbComponents: {
+                files: [
+                    {src: 'asb-components.clean.js', dest: 'asb-components.min.js'}
+                ]
+            }
+        },
+
+        karma: {
+            unit: {
+                configFile: 'my.karma.js',
+                background: true,
+                browsers: ['Chrome']
+            },
+
+            continuous: {
+                configFile: 'my.karma.js',
+                singleRun: true,
+                browsers: ['Chrome']
+            }
+        }
+    });
+
+    grunt.registerTask(
+        'default',
+        [
+            'jshint:asbComponents', 'clean:asbComponents', 'concat:asbComponents', 'groundskeeper:asbComponents',
+            'uglify:asbComponents', 'karma:continuous'
+        ]
+    );
+};
